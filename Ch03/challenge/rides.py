2f2feccbd7917a783c5161fda3213d51d4d516f4
Colton Matthews <colton.matthews17@gmail.com>
2025-03-18 19:48:12 -0400

    Going through it!

--- Ch03/challenge/rides.py
+++ Ch03/challenge/rides.py
@@ -8,3 +8,23 @@ df
 # - Missing values are not allowed
 # - A plate must be a combination of at least 3 upper case letters or digits
 # - Distance much be bigger than 0
+
+import pandera as pa
+
+#schema
+schema = pa.DataFrameSchema({
+    'name': pa.Column(pa.String, nullable=False),
+    'plate': pa.Column(
+        pa.String,
+        checks=pa.Check.str_matches(r'^[A-Z0-9]{3,}$')
+    ),
+    'distance': pa.Column(
+        pa.Float,
+        checks=pa.Check(lambda v: v > 0, 
+                        element_wise=True),
+        element_wise=True,
+    ),
+})
+
+df
+# %%
